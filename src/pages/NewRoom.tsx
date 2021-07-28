10223df8ab32210445ef8a37867b8f8beaa18e75
windel <windel@debianwindel.windel.local>
2021-07-28 11:59:26 -0300

    Adicionando pagina de perguntas

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,18 +1,41 @@
-import { Link } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
 
+import { Link, useHistory } from 'react-router-dom';
 import { Form } from 'react-bootstrap';
+
 import illustration from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
-//import { useContext } from 'react';
-//import { AuthContext } from '../contexts/AuthContext';
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
+
 
 
 export function NewRoom(){
-	//const {user} = useContext(AuthContext);
+	const { user } = useAuth();
+	const history = useHistory();
+	const [newRoom, setNewRoom] = useState('');
 
+	async function handleCreateRoom(event: FormEvent) {
+		event.preventDefault();
+
+		if (newRoom.trim() === '') {
+			return;
+		}
+
+		const roomRef = database.ref('rooms');
+
+		const firebaseRoom = await roomRef.push({
+			title: newRoom,
+			authorId: user?.id,
+			authorName: user?.name,
+
+		})
+
+		history.push(`/rooms/${firebaseRoom.key}`)
+	}
 
 	return(
 		<div id="page-auth">
@@ -26,10 +49,12 @@ export function NewRoom(){
 				<div className="main-content">
 					<img src={logoImg} alt="Letmeask"></img>
                     <h2>Criar uma nova sala</h2>
-					<Form>
+					<Form onSubmit={handleCreateRoom}>
 						<input
 							type="text"
 							placeholder="Nome da sala"
+							onChange={event => setNewRoom(event.target.value)}
+							value={newRoom}
 						/>
 						<Button type="submit">
 							Criar sala
